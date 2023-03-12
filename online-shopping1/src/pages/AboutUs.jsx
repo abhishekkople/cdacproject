2b8e298973dce99170b198addfa7b697a503287b
PH10_65800_Abhishek <er.abhishekkople@gmail.com>
2023-03-12 20:54:50 +0530

    after final submission commit

---
+++ online-shopping1/src/pages/AboutUs.jsx
@@ -0,0 +1,22 @@
+
+
+const AboutUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>
+        <h1>Project Members</h1><br/><br/>
+     <h2>Name: Shweta Singh</h2><b/>
+     <h2>Name: Abhishek Kople</h2>
+     <h2>Name: Vidya Navale</h2>
+     <h2>Name: Harshada Mahajan</h2><br/><br/><br/>
+     <br/>
+     <h2>Institute Name:</h2>
+     <h3>Infoway Technologies,<br/>
+Infoway is authorized C-DAC and C-CAT for PG DAC training centre in Pune.</h3>
+
+      </b>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ online-shopping1/src/pages/ContactUs.jsx
@@ -0,0 +1,15 @@
+
+const ContactUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>
+      <h3>Contact Us:</h3>
+          <h1> Tollfree number:20019436887859<br/><br/>
+           Mail Id:Vintage_Store121@gmail.com
+           </h1> 
+      </b>
+    </div>
+  );
+};
+
+export default ContactUs;
